ab74c8864d9516d424fc02e4294181279cba8a08
Tizon <tivetind23@hotmail.com>
2017-03-22 16:21:46 -0500

    fix routes

--- NNRobotUSup/Network/Routes.py
+++ NNRobotUSup/Network/Routes.py
@@ -1,33 +1,54 @@
 import requests as req
 import threading as t
-class Network:
 
-    def __init__(self,debug):
+class Network:
+    def __init__(self, debug):
         self.debug = debug
         print "request created"
 
-    def sendData(self,data,posData,logic,image,explore,cpu,memory):
-        logmessage = str(logic) + "#" + str(len(posData)) + "#" + str(image) + "#" + str(explore) + "#" + str(cpu) + "#" + str(memory)
+    def sendData(self, data, posData, logic, image, explore, cpu, memory):
+        logmessage = str(logic) + "#" + str(len(posData)) + "#" + str(image) + "#" + str(
+            explore) + "#" + str(cpu) + "#" + str(memory)
         self.logRoutes(str(logmessage))
-        tPost = t.Thread(target=self.asycnSend('http://190.158.131.76:3000/data',data,posData,logic,image,explore,cpu,memory))
+        tPost = t.Thread(
+            target=self.asycnSend('http://190.158.131.76:3000/data', data, posData, logic, image, explore,
+                                  cpu, memory))
         tPost.start();
 
-    def sendDataSight(self,moments,cluster1,cluster2,center):
+    def sendDataSight(self, moments, cluster1, cluster2, center):
         self.logRoutes("Camera Data")
-        tPost = t.Thread(target=self.asycnSend('http://190.158.131.76:3000/dataSigth',moments, cluster1,cluster2,center))
+        tPost = t.Thread(
+            target=self.asycnSendSight('http://190.158.131.76:3000/dataSigth', moments, cluster1, cluster2,
+                                  center))
         tPost.start();
 
-    def asycnSend(self,url,moments,cluster1,cluster2,center):
+    def asycnSend(self,url,data,posData,logic,image,explore,cpu,memory):
         r= req.post(url,data = {
-            'moments':moments,
-            'cluster1': cluster1,
-            'cluster2':cluster2,
-            'center':center
+            'buffer':data,
+            'bufferpos': [posData],
+            'nlogic':logic,
+            'nimage':image,
+            'nexplore':explore,
+            'cpu':cpu,
+            'memory':memory
         })
         self.logRoutes("Send Laser: "+r.text)
 
+    def asycnSendSight(self, url, moments, cluster1, cluster2, center):
+        r = req.post(url, data={
+            'moments': moments,
+            'cluster1': cluster1,
+            'cluster2': cluster2,
+            'center': center
+        })
+        self.logRoutes("Send Laser: " + r.text)
 
 
     def logRoutes(self,message):
         if self.debug:
             print message
+
+
+
+
+
