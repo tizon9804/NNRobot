f5175afabd86b71c125aa254872d4b9fc4edd7fa
Tizon <tivetind23@hotmail.com>
2017-05-01 16:39:07 -0500

    conection neural network and visualization. camera web streaming

--- NNRobotUSup/Network/FtpSender.py
+++ NNRobotUSup/Network/FtpSender.py
@@ -7,21 +7,26 @@ from base64 import b64encode
 class FtpSender:
     def __init__(self):
         self.connect()
+        self.sending = False
 
     def connect(self):
-        #self.sock = SocketIO('104.198.238.71',80,LoggingNamespace)
-        self.sock = SocketIO('190.158.131.76', 3000, LoggingNamespace)
+        self.sock = SocketIO('104.198.238.71',80,LoggingNamespace)
+        #self.sock = SocketIO('190.158.131.76', 3000, LoggingNamespace)
         self.isconnect = True
     def upload(self,img,name):
         if self.isconnect:
-            t = threading.Thread(target=self.uploadasync, args=(img, name))
-            t.start()
+            if not self.sending:
+                t = threading.Thread(target=self.uploadasync, args=(img, name))
+                t.start()
+
         else:
             self.connect()
 
     def uploadasync(self,img,name):
+        self.sending = True
         img = cv2.imencode('.jpeg', img)
         self.sock.emit(name, {'buffer': b64encode(img[1])})
+        self.sending = False
 
 
 
