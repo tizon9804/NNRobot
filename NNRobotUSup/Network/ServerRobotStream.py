02a86f419c7c1614d9660083fc144f2f7ae008bf
Stevenson Contreras <tivetind23@hotmail.com>
2017-12-09 20:41:42 -0500

    change read and send data sockets

--- NNRobotUSup/Network/ServerRobotStream.py
+++ NNRobotUSup/Network/ServerRobotStream.py
@@ -2,6 +2,7 @@ import socket
 import pickle
 import numpy as np
 import time
+import struct
 class RobotServerStream:
     def __init__(self,bparm):
         # Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
@@ -21,19 +22,22 @@ class RobotServerStream:
         waiting=True
         while(waiting):
             print '#################### Waiting Robot...########################'
-            data = self.getData()
+            data = self.getData().decode()
             if data == 'raspChappie':
                 print data
                 self.setData('HiRasp')
-                self.getData()
+                self.getData().decode()
                 waiting = False
                 self.isRobotActive = True
                 print '####################Started Robot...########################'
 
     def getData(self):
         try:
-            connection = self.connection
-            data = connection.recv(1024).decode()
+            dataLen = self.recvall(4)
+            if not dataLen:
+                return None
+            dataLen = struct.unpack('>I',data)[0]
+            data = self.recvall(dataLen)
             return data
         except Exception as ex:
             print "Network: finalizo streaming: " + str(ex)
@@ -43,11 +47,22 @@ class RobotServerStream:
             self.bparm.senseLife = False
             self.connection.close()
             self.server_socket.close()
-            return '#############################################################'
-
+            return None
+        
+    def recvall(n):
+        # Helper function to recv n bytes or return None if EOF is hit
+        data = b''
+        while len(data) < n:
+            packet = self.connection.recv(n - len(data))
+            if not packet:
+                return None
+            data += packet
+        return data
+    
     def setData(self,data):
         try:
-            self.connection.send(b''+str(data))
+            msg = struct.pack('>I'),len(data) + data)
+            self.connection.sendall(msg)
         except Exception,ex:
             print "Robot Error: ",str(ex)
             self.isRobotActive = False
@@ -62,17 +77,7 @@ class RobotServerStream:
             time.sleep(2)
             data = 'getClosestDistance:'+str(x)+':'+ str(xend)
             self.setData(data)
-            data = []
-            while True:
-                packet = self.connection.recv(1024)
-                print "packet"
-                print len(packet)                
-                if not packet: break
-                data.append(packet)
-                print "pre daata"
-                print len(data)
-            print "data#####################"
-            print len(data)           
+            data = self.getData()           
             lmoves = pickle.loads(b"".join(data))
             return lmoves
         except Exception,ex:
@@ -83,39 +88,39 @@ class RobotServerStream:
         time.sleep(1)
         data = 'isHeadingDone'
         self.setData(data)
-        return bool(self.getData())
+        return bool(self.getData().decode())
 
 
     def rotate(self,angle):
         time.sleep(1)
         data = 'rotate:' + str(angle)
         self.setData(data)
-        self.getData()
+        self.getData().decode()
 
     def rotateSecure(self,angle):
         time.sleep(1)
         data = 'rotateSecure:' + str(angle)
         self.setData(data)
-        self.getData()
+        self.getData().decode()
 
     def restartHeading(self):
         time.sleep(1)
         data = 'restartHeading'
         self.setData(data)
-        self.getData()
+        self.getData().decode()
 
     def getTh(self):
         time.sleep(1)
         data = 'getTh'
         self.setData(data)
-        return float(self.getData())
+        return float(self.getData().decode())
 
     def getClosesFrontDistance(self):
         try:
             time.sleep(1)
             data = 'getClosestFrontDistance'
             self.setData(data)
-            closest= float(self.getData())
+            closest= float(self.getData().decode())
             return closest
         except Exception, ex:
             print "Falla: intentando enviar getClosestFrontDistance",str(ex)
@@ -124,25 +129,13 @@ class RobotServerStream:
         time.sleep(1)
         data = 'move:' + str(dist)
         self.setData(data)
-        self.getData()
+        self.getData().decode()
 
     def getLaserBuffer(self):                    
         data = 'getLaserBuffer'
         print data
         self.setData(data)
-        data = []
-        print "enter while true"
-        while True:
-            print "looping"
-            packet = self.connection.recv(1024)
-            print "packet"
-            print len(packet)                
-            if not packet: break
-            data.append(packet)
-            print "pre daata"
-            print len(data)
-        print "data#####################"
-        print len(data)
+        data = self.getData()
         data_arr,posData = pickle.loads(b"".join(data))
         return np.array(data_arr),posData
        
@@ -150,10 +143,10 @@ class RobotServerStream:
     def getMaxReadings(self):
         data = 'getMaxReadings'
         self.setData(data)
-        return int(self.getData())
+        return int(self.getData().decode())
 
     def getMaxDistance(self):
         data = 'getMaxDistance'
         self.setData(data)
-        return int(self.getData())
+        return int(self.getData().decode())
 
